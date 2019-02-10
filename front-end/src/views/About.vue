52537c8d76afd9d70dfc3e4438e5415b5ac3a534
ifmoon <ehdals1992@naver.com>
2019-02-10 14:22:57 +0900

    express 추가 & express와 vue 연동

--- front-end/src/views/About.vue
+++ front-end/src/views/About.vue
@@ -1,5 +1,29 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button v-on:click="test_func">TEST</button>
   </div>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      test: ''
+    }
+  },
+  methods: {
+    test_func: function () {
+      this.$http.post('/api/user/login',{id:'test', password:'1434'})
+      .then((response) => {
+        alert(response.data.message)
+      })
+    }
+  },
+  created () {
+    this.$http.get('/api/user/login')
+    .then((response) => {
+      this.test = response.data
+    })
+  }
+}
+</script>
